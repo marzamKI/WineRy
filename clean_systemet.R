5304cb3c2c0a1356431d5041dcb12f79b605282d
Margherita Zamboni <margherita.zamboni@ki.se>
2019-06-18 12:16:47 +0200

    clean systemet dataset

---
+++ clean_systemet.R
@@ -0,0 +1,20 @@
+systemet <- read.csv("systemet.csv", header = T, sep = ";")
+
+#Get wine
+vin <- grep("vin", x = systemet$Varugrupp, value = T, ignore.case = T)
+
+#Get single bottles/boxes
+a_fl <- c(grep("??", x = systemet$Forpackning, value = T, ignore.case = T),
+          grep("??", x = systemet$Forpackning, value = T, ignore.case = T))
+
+systemet_vin <- systemet %>% 
+  filter(Varugrupp %in% vin) %>%
+  filter(!Forpackning %in% a_fl) %>%
+  filter(!Typ %in% c("Vodka", "Kryddat br??nnvin", "3 flaskor")) %>%
+  select(-c("Saljstart", "Pant", "Leverantor", 
+            "nr", "Varnummer", "Utg??tt", "Stil",
+            "Provadargang", "Sortiment", "SortimentText"))
+
+systemet_vin$APK <- parse_number(as.character(systemet_vin$Alkoholhalt))/systemet_vin$PrisPerLiter
+
+write.csv(systemet_vin, "systemet_vin.csv")
