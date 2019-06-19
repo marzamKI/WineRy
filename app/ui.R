bdc4c66adb3460679a5584aa2a807bfc7d201620
Stephanie Herman <stephanie.herman@live.se>
2019-06-19 09:35:32 +0200

    udate ui.R

--- app/ui.R
+++ app/ui.R
@@ -6,30 +6,39 @@ wine_map <- read.csv("../data/wine_map.csv", header = T)
 ui <- fluidPage(
   titlePanel("WineRy"),
   sidebarLayout(
-    sidebarPanel(tableOutput("table"),
-                 selectInput("in_title", label="Select country",choices = vino$country),
-                 selectizeInput("variety", "Select variety", vino$variety),
-                 br(),
-                 splitLayout(cellWidths = c("50%", "50%"),
-                             selectInput("X", label="X Axis", choices = NULL),
-                             selectInput("Y", label="Y Axis", choices = NULL)),
-                 br(),
-                 br(),
-                 actionButton("btn_go","Go!"),
-                 hr(),
-                 plotOutput("full_plot"),
-                 hr(),
-                 submitButton("Print shopping list")),
+    sidebarPanel(
+      tableOutput("table"),
+      h3('Settings'),
+      fluidRow(
+        column(6,style=list("padding-right: 5px;"),
+               selectInput("in_title", label="Select country",choices = vino$country)
+        ),
+        column(6,style=list("padding-left: 5px;"),
+               selectizeInput("variety", "Select variety", vino$variety)
+        )),
+      fluidRow(
+        column(6,style=list("padding-right: 5px;"),
+               selectInput("X", label="X Axis", choices = NULL)
+        ),
+        column(6,style=list("padding-left: 5px;"),
+               selectInput("Y", label="Y Axis", choices = NULL)
+        )),
+      tags$br(),
+      actionButton("btn_go","Go!"),
+      tags$hr(),
+      plotOutput("full_plot"),
+      tags$hr(),
+      submitButton("Print shopping list")),
     
     mainPanel(
       selectInput("map_input", 
                   label="Select input",
                   choices = c("mean_price", "mean_point", "size")),
       plotOutput("map"),
-              splitLayout(cellWidths = c("50%", "50%"),
-                          plotOutput("price_plot")),
-              plotOutput("spider")
-              )
+      splitLayout(cellWidths = c("50%", "50%"),
+                  plotOutput("price_plot")),
+      plotOutput("spider")
     )
+  )
 )
-  
+
