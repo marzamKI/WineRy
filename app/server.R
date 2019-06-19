97ea5d2991d8d4f02c689a6a2661e2e1aaae7f35
Stephanie Herman <stephanie.herman@live.se>
2019-06-19 16:28:11 +0200

    fixed click event in scatter plot

--- app/server.R
+++ app/server.R
@@ -46,42 +46,57 @@ server=function(input,output) {
       arrange(desc(n)) %>% 
       top_n(n=3, wt=n)
     
-      ggplotly(
-      vino %>% 
+
+      dat <- vino %>% 
       filter(country == input$in_title) %>% 
       group_by(variety) %>% 
       select(x=input$X,y=input$Y) %>% 
       mutate(color = case_when(variety == top[[1,1]] ~ paste("1",variety, sep = " "),
                                variety == top[[2,1]] ~ paste("2",variety, sep = " "),
                                variety == top[[3,1]] ~ paste("3",variety, sep = " "),
-                               TRUE ~ "etc.")) %>% 
-      ggplot(aes(x = x, y = y))+
-        geom_jitter(aes(fill= color),
-                  width = 0.2, height = 0.2, 
-                  shape=21,size=3,stroke=0.8,
-                  #fill="white",
-                  color = 'black',
-                  show.legend = TRUE)+
-        geom_smooth(method="loess",se=F,color="red",size=0.6, show.legend = FALSE)+
-      labs(title = paste("Wine", input$X, "per", input$Y, sep=" "))+
-      xlab(paste(input$X))+
-      ylab(paste(input$Y, "(USD)", sep = " ")) +
-      #xlim(0,5)+
-      guides(color = guide_legend(nrow = 2))+
-      scale_fill_manual(values = c("red", "orange", "yellow", "grey60"))+
-      theme(legend.position = "bottom",
-            panel.grid.minor=element_blank(),
-            panel.grid.major.x=element_blank(),
-            panel.background=element_blank(),
-            panel.border=element_blank(),
-            legend.title=element_blank(),
-            axis.title=element_text(face="italic"),
-            axis.ticks.y=element_blank(),
-            axis.ticks.x=element_line(color="grey60"),
-            plot.title=element_text(face="bold", hjust=0.5)))
+                               TRUE ~ "etc."))
+      dat <- na.omit(dat)
+      dat %>% plot_ly(x = ~x, y = ~y, type = "scatter", source = 'select',
+                      color = ~color, colors = c("red", "orange", "yellow", "grey60"),
+                      marker = list(size = 10,
+                                    line = list(color = 'rgba(0, 0, 0, .8)',
+                                                width = 2))) %>%
+        add_lines(y = ~fitted(loess(y ~ x)),
+                  line = list(color = '#07A4B5'),
+                  name = "Loess Smoother", showlegend = TRUE) %>%
+                        layout(xaxis = list(title = paste(input$X)),
+                               yaxis = list(title = paste(input$Y, "(USD)", sep = " "))
+                               )
+      
+      # ggplot(data = dat, aes(x = x, y = y), source = "select")+
+      #   geom_jitter(aes(fill= color),
+      #             width = 0.2, height = 0.2, 
+      #             shape=21,size=3,stroke=0.8,
+      #             color = 'black',
+      #             show.legend = TRUE)+
+      #   geom_smooth(method="loess",se=F,color="red",size=0.6, show.legend = FALSE)+
+      # labs(title = paste("Wine", input$X, "per", input$Y, sep=" "))+
+      # xlab(paste(input$X))+
+      # ylab(paste(input$Y, "(USD)", sep = " ")) +
+      # guides(color = guide_legend(nrow = 2))+
+      # scale_fill_manual(values = c("red", "orange", "yellow", "grey60"))+
+      # theme(legend.position = "bottom",
+      #       panel.grid.minor=element_blank(),
+      #       panel.grid.major.x=element_blank(),
+      #       panel.background=element_blank(),
+      #       panel.border=element_blank(),
+      #       legend.title=element_blank(),
+      #       axis.title=element_text(face="italic"),
+      #       axis.ticks.y=element_blank(),
+      #       axis.ticks.x=element_line(color="grey60"),
+      #       plot.title=element_text(face="bold", hjust=0.5))
             })
   
   output$spider <- renderPlot ({
+    s <- event_data("plotly_click", source = "select")
+    vars <- c(s[["x"]], s[["y"]])
+    return(as.character(vars))
+    
     names(demo) <- gsub("taste_", "", names(demo))
     substr(names(demo), 1, 1) <- toupper(substr(names(demo), 1, 1))
     
@@ -99,5 +114,9 @@ server=function(input,output) {
                vlcex=0.8 )
   })
   
-  output$descr <- renderText({ 'The description will go here'})
+  output$descr <- renderText({ 
+    s <- event_data("plotly_click", source = "select")
+    vars <- c(s[["x"]], s[["y"]])
+    return(as.character(vars))
+  })
 }
